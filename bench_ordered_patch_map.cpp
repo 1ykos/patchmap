1218af434ba9824a1518c845ce0700f0dac6a0d9
Wolfgang Brehm <wolfgang.brehm@gmail.com>
2019-04-03 15:07:21 +0200

    benchmark used in publication

--- bench_ordered_patch_map.cpp
+++ bench_ordered_patch_map.cpp
@@ -1,86 +1,175 @@
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <limits>
-#include <cmath>
+#include <map>
 #include <random>
-#include <bitset>
-#include <iomanip>
+#include <string>
 #include <unordered_map>
-#include <chrono>
 #include "ordered_patch_map.hpp"
+#include "flat_hash_map.hpp"
+#include "bytell_hash_map.hpp"
 #include "sparsepp/spp.h"
+#include <sparsehash/sparse_hash_map>
+#include <sparsehash/dense_hash_map>
+#include "khash.h"
+
+#include <stdio.h>
+#include <proc/readproc.h>
 
 using wmath::ordered_patch_map;
 using wmath::reflect;
+using wmath::rol;
 
 using std::cout;
 using std::endl;
 using std::bitset;
 using std::setw;
 using std::get;
+using std::stoi;
+using std::max;
 
-int main(){
+//#define PATCHMAP
+//#define SPARSEPP
+//#define KHASH
+//#define UNORDERED_MAP
+//#define BYTELL
+//#define FLATMAP
+//#define SPARSEMAP
+//#define MAP
+
+#ifdef KHASH
+KHASH_MAP_INIT_INT(32, uint32_t)
+#endif
+
+uint32_t gen_rand(uint32_t i){
+  return i*3063032679ull;
+}
+
+int main(int argc, char** argv){
   std::ios_base::sync_with_stdio(false);
+  if (argc<2) return 1;
+  const size_t N = stoi(argv[1]);
   std::random_device urand;
-  std::ranlux48 mr(urand());
-  std::uniform_int_distribution<uint32_t> u32distr(1u<<27);
-  /*for (size_t i=0;i!=256;++i){
-    uint8_t a[256]{};
-    for (size_t j=0;j!=256;++j){
-      const uint8_t m = wmath::circmul(uint8_t(j),uint8_t(j));
-      const uint8_t n = (j+1)*j;
-      ++a[uint8_t(n+m)];
-      //cout << int(uint8_t(n+m)) << endl;
-      //if (n!=m){//cout << int(n) << " " << int(m) << endl;
-        cout << i << " " <<  j << " = " << i*j << endl;
-        cout << uint(get<0>(r)) << " " << (i*j)%256 << endl
-             << uint(get<1>(r)) << " " << (i*j)/256 << endl;
-      //}
-    }
-    size_t n =0;
-    for (size_t j=0;j!=256;++j) n+=(a[j]>0);
-    cout << "::" << int(i) << " " << int(n) << endl;
+  std::minstd_rand mr(urand());
+  std::uniform_int_distribution<uint32_t> u32distr(1u<<30);
+  double initial_memory;
+  {
+    struct proc_t usage;
+    look_up_our_self(&usage);
+    initial_memory = usage.vsize;
   }
-  return 0;*/
+#ifdef PATCHMAP
   ordered_patch_map<uint32_t,uint32_t> test;
-  //spp::sparse_hash_map<uint32_t,uint32_t> test;
-  //ordered_patch_map<size_t,size_t,wmath::insecure_hash_functor<size_t>> test(32);
-  //std::unordered_map<size_t,size_t> test;
+#endif
+#ifdef SPARSEPP
+  spp::sparse_hash_map<uint32_t,uint32_t> test;
+#endif
+#ifdef UNORDERED_MAP
+  std::unordered_map<uint32_t,uint32_t> test;
+#endif
+#ifdef MAP
+  std::map<size_t,size_t> test;
+#endif
+#ifdef FLATMAP
+  ska::flat_hash_map<uint32_t,uint32_t> test;
+#endif
+#ifdef BYTELL
+  ska::bytell_hash_map<uint32_t,uint32_t> test;
+#endif
+#ifdef SPARSEMAP
+  google::sparse_hash_map<uint32_t,uint32_t> test;
+#endif
+#ifdef KHASH
+  khash_t(32) *h = kh_init(32);
+  int ret, is_missing;
+  khiter_t k;
+#endif
+  int sand = 0;
   std::uniform_int_distribution<size_t> distr;
-  uint32_t * precompute0 = new uint32_t[67108864ul];
-  uint32_t * precompute1 = new uint32_t[67108864ul];
-  for (size_t i=0;i!=67108864ull;++i) precompute0[i]=precompute1[i]=u32distr(mr);
-  std::shuffle(precompute1,precompute1+67108864ul,mr);
-  uint32_t * cache0 = new uint32_t[256];
-  uint32_t * cache1 = new uint32_t[256];
-  for (size_t i=0;i!=67108864ul;i+=256){
-    for (size_t j=0;j!=256;++j){
-      cache0[j]=i+j;//precompute0[i+j];
-      cache1[j]=i+j;//precompute1[i+j];
+  double counter = 0;
+  double acc_insert = 0;
+  double acc_find = 0;
+  double acc_delete = 0;
+  double acc_not_find = 0;
+  double typical_insert_time = 0;
+  double typical_delete_time = 0;
+  double typical_find_time = 0;
+  double typical_not_find_time = 0;
+  double typical_memory = 0;
+  auto start = std::clock();
+  for (size_t i=0;i!=N/4096;++i) {
+    auto start = std::clock();
+    for (size_t j=0;j!=4096;++j) {
+      const uint32_t n = gen_rand(2*(i*4096+j));
+#ifdef KHASH
+      k = kh_put(32, h, n, &ret);
+      kh_value(h, k) = n;
+#else
+      test[n]=n;
+#endif
     }
-    auto start = std::chrono::high_resolution_clock::now();
-    for (size_t j=0;j!=256;++j){
-      const uint32_t r = cache0[j]; // reverse(i+j);// distr(mr);
-      test[r]=r;
+    typical_insert_time+=(acc_insert+=(std::clock()-start))/(i+1);
+    struct proc_t usage;
+    look_up_our_self(&usage);
+    typical_memory+=(double(usage.vsize)-initial_memory)/(i+1);
+    mr.seed(i+7ul);
+    std::uniform_int_distribution<size_t> distr(0,i);
+    const size_t l0 = distr(mr);
+    start = std::clock();
+    for (size_t j=0;j!=4096;++j) {
+      const uint32_t n = gen_rand(2*(l0*4096+j));
+#ifdef KHASH
+      k = kh_get(32, h, n);
+      sand += k;
+#else
+      sand+=test.count(n);
+#endif
     }
-    auto finish = std::chrono::high_resolution_clock::now();
-    cout << i << " " << (finish-start).count() << " ";
-    start = finish;
-    size_t n = 0;
-    for (size_t j=0;j!=256;++j){
-      const uint32_t r = cache1[j];
-      /*if (r%3==0){
-        if (!test.erase(r)){
-          cout << "map broken (element not found)" << endl;
-          return 1;
-        }
-      }*/
-      n+=test.count(r);
+    typical_find_time+=(acc_find+=(std::clock()-start))/(i+1);
+    const size_t l1 = distr(mr);
+    start = std::clock();
+    for (size_t j=0;j!=4096;++j) {
+      const uint32_t n = gen_rand(2*(l1*4096+j)+1);
+#ifdef KHASH
+      k = kh_get(32, h, n);
+      sand += k;
+#else
+      sand+=test.count(n);
+#endif
     }
-    finish = std::chrono::high_resolution_clock::now();
-    //const double a = test.average_offset();
-    cout << (finish-start).count() << " " << test.size() << " "
-         << test.load_factor() << " " << n << endl;
-    start = finish;
+    typical_not_find_time+=(acc_not_find+=(std::clock()-start))/(i+1);
+    const size_t l2 = distr(mr);
+    start = std::clock();
+    for (size_t j=0;j!=4096;++j){
+      const uint32_t n = gen_rand(2*(l2*4096+j));
+#ifdef KHASH
+      k = kh_get(32, h, n);
+      if (k!=kh_end(h)) kh_del(32, h, k);
+#else
+      test.erase(n);
+#endif
+    }
+    typical_delete_time+=(acc_delete+=(std::clock()-start))/(i+1);
+    for (size_t j=0;j!=4096;++j){
+      const uint32_t n = gen_rand(2*(l2*4096+j));
+#ifdef KHASH
+      k = kh_put(32, h, n, &ret);
+      kh_value(h, k) = n;
+#else
+      test[n]=n;
+#endif      
+    }
   }
-  return 0;
+  cout << typical_memory/N        << " "
+       << typical_insert_time/N   << " "
+       << typical_delete_time/N   << " "
+       << typical_find_time/N     << " "
+       << typical_not_find_time/N << endl;
+#ifdef KHASH
+  kh_destroy(32, h);
+#endif
+  return sand;
 }
