a3c750a26c1fa326e54741ad82657755b7214714
Wolfgang Brehm <wolfgang.brehm@gmail.com>
2019-04-03 15:07:52 +0200

    simple test to see if the hash map at least works

--- bigtest_ordered_patch_map.cpp
+++ bigtest_ordered_patch_map.cpp
@@ -1,11 +1,11 @@
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <limits>
-#include <cmath>
 #include <random>
-#include <bitset>
-#include <iomanip>
 #include <unordered_map>
-#include <chrono>
 #include "ordered_patch_map.hpp"
 
 using wmath::ordered_patch_map;
@@ -22,33 +22,42 @@ using std::tuple;
 using std::get;
 using std::allocator_traits;
 
+uint32_t gen_rand(uint32_t i){
+  return i*2694261069ull;
+  return wmath::rol(i*3063032679ull,13)*2694261069ull;
+}
 
 int main(){
-  const uint32_t N = 1u<<24; // fails when > 2¹⁷ bc. inversed breaks
+  const uint32_t N = 1u<<27;
+  std::minstd_rand mr;
   ordered_patch_map<uint32_t,uint32_t> test;
+  //std::unordered_map<uint32_t,uint32_t> test;
   cout << "sizeof(ordered_patch_map{}) = " << sizeof(test) << endl;
   for (uint32_t i=0;i!=N;++i){
-    const uint32_t j = i*7;
+    const uint32_t j = gen_rand(i);
     test[j]=i;
-    const uint32_t k = (2*i/3)*7;
+    std::uniform_int_distribution<uint32_t> u32distr1(0,i);
+    const uint32_t k = gen_rand(u32distr1(mr));
     if (test.count(k)==0) {
       //test.print();
       cout << "could not retrieve key that should be there" << endl;
       cout << wmath::frac(wmath::distribute(k)) << endl;
       return 1;
     }
-    const uint32_t l = uint32_t(0)-1-i;
+    std::uniform_int_distribution<uint32_t> u32distr0(i+1,~uint32_t(0));
+    const uint32_t l = gen_rand(u32distr0(mr));
     if (test.count(l)){
       cout << "found something that should not be there" << endl;
       cout << l << " " << wmath::frac(wmath::distribute(k)) << endl;
       return 1;
     }
   }
+  /*
   if (!test.check_ordering()){
-    test.print();
+    //test.print();
     cout << "ordering is broken (2)" << endl;
     return 1;
-  }
+  }*/
   //test.print();
   cout << "test was successfull" << endl;
   return 0;
